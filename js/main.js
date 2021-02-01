2bf260b00e8e5e76574a5a27953da7fdb052a08a
Fabbri <theofabbri@yahoo.fr>
2021-02-01 17:48:19 +0100

    enigme 3

--- js/main.js
+++ js/main.js
@@ -20,5 +20,18 @@ function launchgame() {
     var fiveMinutes = 60 * 20,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
+    
+    // initialisation du projet coté egnime 3
+    let b = document.getElementById('debut');
+    b.innerHTML=' ';
+    b.innerHTML="<button type='button' onclick='changeHTML()' class='btn btn-secondary btn-lg launchbutton'>Deverouiller l'énigme 3</button>";
+}
+
+function changeHTML(){
+    let a = document.getElementById('debut');
+    console.log('QZLJ');
+    a.innerHTML=' ';  
+
+    a.innerHTML="<p class='texte_egnime_3'>Il vous faudra visiter le campus pour trouver la salle en face de Superman et de noté son chiffre dans un coin de vôtre tête.</br>A noté qu'une chose c'est passé dans cette page, il se trouve dans l'outils du navigateur préféré des développeurs web</p>";
 };
 
