efbd90e1844885ee4a8e11488c7a5e1ee20b20cf
Fabrice <fabrice.goncalves2@gmail.com>
2021-02-01 18:18:36 +0100

    Ajout du pendu, recuperer le code de pendu.html pour le mettre dans le bon innerHTML dans main.js a la fin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // Timer réglé sur 20 min. TODO : innerhtml page défaite si arrivé à zero.
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -22,3 +23,68 @@ function launchgame() {
     startTimer(fiveMinutes, display);
 };
 
+
+//fonctions pour le pendu :
+let okValues ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+function VerifValeur(lettre,tab){
+    let bool = false;
+    for (const valeur in tab) {
+        bool = lettre == tab[valeur];
+        if (bool){
+            break
+        }
+    }
+    if(!bool){
+        valeur.value=''
+    }
+}
+let echec = 1
+let Mot ='ada lovelace';
+let MotVide = ['_','_','_',' ','_','_','_','_','_','_','_']
+function RechercheTab(lettre,tab) {
+    tentative.innerHTML = tentative.innerHTML+' '+lettre
+    let bool = false;
+    for (const valeur in tab) {
+        bool = lettre == tab[valeur];
+        if (bool) {
+            break
+        }
+    }
+    if (!bool) {
+        document.getElementById(echec).style.visibility = "visible"
+        echec++;
+        if (echec > 5) {
+            validation.disabled = 'true'
+            validation.style.display='none'
+            valeur.style.display='none'
+            resultat.style.display='inline'
+            resultat.innerHTML='Vous avez perdu, vous recevez un malus de 2 minutes'
+            suivant.style.display='inline'
+        }
+    } else {
+        for (const valeur in tab) {
+            bool = lettre == tab[valeur];
+            if (bool) {
+               MotVide[valeur] = lettre
+                bool = false
+            }
+        }
+        for (const valeur in MotVide){
+        var motComplet =MotVide.toString().replace(/([,])/g,'')
+        }
+        if(motComplet ==Mot){
+            validation.disabled = 'true'
+            validation.style.display='none'
+            valeur.style.display='none'
+            resultat.style.display='inline'
+            resultat.innerHTML='Vous avez gagné, vous pouvez passer a la suite'
+            suivant.style.display='inline'
+        }
+        pendu.innerHTML = motComplet;
+    }
+}
+function next(){
+    divchange.innerHTML="";
+    divchange.innerHTML=""//mettre le innerHTML de l'enigme suivante
+
+}
